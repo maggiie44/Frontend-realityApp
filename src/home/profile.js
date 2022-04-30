72e2c7d4321b2e36f77c4d5ea5d7dfe233f1477c
kunanon Mahatanasombat <maggiie44@gmail.com>
2022-04-30 13:00:32 +0700

    test

--- src/home/profile.js
+++ src/home/profile.js
@@ -43,7 +43,7 @@ function Profile(props) {
           if(isMounted) setData(res.data);      
         });
         return () => { isMounted = false}
-    },[data]);
+    },[]);
   
     const handleProfileChange = e => {
       setProfileData({
